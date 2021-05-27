514b29fce2191f39735d43b775e7071f1a5bbe64
Ulada Haravaya <Ulada.Haravaya@itechart-group.com>
2021-05-27 11:40:36 +0300

    add userInfo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Container } from '@material-ui/core';
 import React from 'react';
 import { FeedAndMyRewards, UserInfo } from './components';
 
 function App() {
   return (
-    <>
-      <UserInfo />
+    <Container maxWidth="md">
+      <UserInfo myRewards={250} give={100} />
       <FeedAndMyRewards />
-    </>
+    </Container>
   );
 }
 
--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,23 +1,51 @@
-import { makeStyles } from "@material-ui/core";
 import React from "react";
+import { makeStyles } from "@material-ui/core";
 import { UserIcon } from "..";
 
 const useStyles = makeStyles({
-    userIconWrapp: {
-        borderRadius: 30,
-        width: 30,
-        height: 30,
+    userInfoWrapp: {
+        display: "flex",
+        width: "100%",
     },
-    icon: {
-
+    userInfoSection: {
+        width: "25%",
+        padding: 20,
+    },
+    userCard: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    userHeader: {
+        margin: "10px 0",
     },
 });
 
-const UserInfo = () => {
+interface UserInfoPops {
+    myRewards: number;
+    give: number;
+}
+
+const UserInfo = ({
+    myRewards,
+    give,
+}: UserInfoPops) => {
     const classes = useStyles();
+
     return (
-        <div>
-            <UserIcon size="large" />
+        <div className={classes.userInfoWrapp}>
+            <div className={`${classes.userInfoSection} ${classes.userCard}`}>
+                <UserIcon size="large" />
+                <h3 className={classes.userHeader}>Jane Doe</h3>
+            </div>
+            <div className={classes.userInfoSection}>
+                <h3 className={classes.userHeader}>My Rewards</h3>
+                <h1 className={classes.userHeader}>${myRewards}</h1>
+            </div>
+            <div className={classes.userInfoSection}>
+                <h3 className={classes.userHeader}>Give</h3>
+                <h1 className={classes.userHeader}>${give}</h1>
+            </div>
         </div>
     );
 };
